34704212b676aa93b03d3a99d327019a3ec384ba
Saurav Sharma <appdroiddeveloper@gmail.com>
2021-03-24 11:52:29 +0545

    Add s3 storages support
    Signed-off-by: Saurav Sharma <appdroiddeveloper@gmail.com>

---
+++ neatplus/storage_backends.py
@@ -0,0 +1,14 @@
+from django.conf import settings
+from storages.backends.s3boto3 import S3Boto3Storage
+
+
+class StaticStorage(S3Boto3Storage):
+    location = settings.STATIC_LOCATION
+    default_acl = "public-read"
+    querystring_auth = False
+
+
+class MediaStorage(S3Boto3Storage):
+    location = settings.MEDIA_LOCATION
+    default_acl = "private"
+    file_overwrite = False
