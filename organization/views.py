a6b5905c56b18d82f059b61709f82c000c22ee19
Saurav Sharma <appdroiddeveloper@gmail.com>
2021-05-11 13:20:28 +0545

    Add API to accept or reject project by org admin
    Signed-off-by: Saurav Sharma <appdroiddeveloper@gmail.com>

--- organization/views.py
+++ organization/views.py
@@ -6,7 +6,7 @@ from rest_framework.response import Response
 from neatplus.views import UserStampedModelUpdateMixin
 
 from .models import Organization, Project
-from .permissions import CanEditProject
+from .permissions import CanEditProjectOrReadOnly, IsProjectOrganizationAdmin
 from .serializers import (
     CreateOrganizationProjectSerializer,
     OrganizationSerializer,
@@ -42,7 +42,7 @@ class ProjectViewSet(
     UserStampedModelUpdateMixin,
     viewsets.GenericViewSet,
 ):
-    permission_classes = [CanEditProject]
+    permission_classes = [CanEditProjectOrReadOnly]
     serializer_class = ProjectSerializer
 
     def get_queryset(self):
@@ -51,12 +51,37 @@ class ProjectViewSet(
             Q(admins=current_user) | Q(members=current_user)
         )
         return Project.objects.filter(
-            Q(visibility="public")
-            | (
-                Q(visibility="public_within_organization")
-                & Q(organization__in=organizations)
-            )
-            | Q(users=current_user)
-            | Q(created_by=current_user)
+            Q(created_by=current_user)
             | Q(organization__admins=current_user)
+            | (
+                (
+                    Q(visibility="public")
+                    | (
+                        Q(visibility="public_within_organization")
+                        & Q(organization__in=organizations)
+                    )
+                    | Q(users=current_user)
+                )
+                & Q(is_accepted_by_admin=True)
+            )
         )
+
+    @action(
+        methods=["post"], detail=True, permission_classes=[IsProjectOrganizationAdmin]
+    )
+    def accept(self, request, *args, **kwargs):
+        project = self.get_object()
+        if not project.is_accepted_by_admin:
+            project.is_accepted_by_admin = True
+            project.save()
+        return Response({"detail": "Project successfully accepted"})
+
+    @action(
+        methods=["post"], detail=True, permission_classes=[IsProjectOrganizationAdmin]
+    )
+    def reject(self, request, *args, **kwargs):
+        project = self.get_object()
+        if not project.is_accepted_by_admin:
+            project.is_accepted_by_admin = False
+            project.save()
+        return Response({"detail": "Project successfully rejected"})
