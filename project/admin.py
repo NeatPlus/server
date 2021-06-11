5de6621b36a44d7d8904850fe5fe829681ce1e42
Saurav Sharma <appdroiddeveloper@gmail.com>
2021-06-11 12:23:11 +0545

    Add feature to accept and reject project from admin
    Signed-off-by: Saurav Sharma <appdroiddeveloper@gmail.com>

--- project/admin.py
+++ project/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.http.response import HttpResponseRedirect
 from ordered_model.admin import OrderedModelAdmin
 
 from neatplus.admin import UserStampedModelAdmin
@@ -18,6 +19,20 @@ class ProjectAdmin(UserStampedModelAdmin, OrderedModelAdmin):
     )
     autocomplete_fields = ("organization", "users", "context")
     search_fields = ("title",)
+    change_form_template = "project_change_form.html"
+
+    def response_change(self, request, obj):
+        if "_reject_project" in request.POST:
+            obj.status = "rejected"
+            obj.save()
+            self.message_user(request, "Project rejected")
+            return HttpResponseRedirect(".")
+        if "_accept_project" in request.POST:
+            obj.status = "accepted"
+            obj.save()
+            self.message_user(request, "Project accepted")
+            return HttpResponseRedirect(".")
+        return super().response_change(request, obj)
 
 
 @admin.register(ProjectUser)
