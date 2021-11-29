c959a503752e649e5a90b5934b637c0ebb2791ef
Saurav Sharma <appdroiddeveloper@gmail.com>
2021-11-29 11:45:40 +0545

    Add VPC flow log to cloudwatch
    Signed-off-by: Saurav Sharma <appdroiddeveloper@gmail.com>

---
+++ terraform/vpc_flow_log.tf
@@ -0,0 +1,60 @@
+# Cloudwatch log group for vpc flow log
+resource "aws_cloudwatch_log_group" "vpc" {
+  name              = "main-${local.suffix}-flow-log"
+  retention_in_days = 90
+}
+
+# IAM role for VPC flow log
+resource "aws_iam_role" "flow_log" {
+  name = "main-${local.suffix}-flow-log-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "vpc-flow-logs.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+# IAM role policy for VPC flow log
+resource "aws_iam_role_policy" "flow_log" {
+  name = "main-${local.suffix}-flow-log-role-policy"
+  role = aws_iam_role.flow_log.id
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents",
+        "logs:DescribeLogGroups",
+        "logs:DescribeLogStreams"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+# Create flow log for VPC
+resource "aws_flow_log" "main" {
+  iam_role_arn         = aws_iam_role.flow_log.arn
+  log_destination_type = "cloud-watch-logs"
+  log_destination      = aws_cloudwatch_log_group.vpc.arn
+  traffic_type         = "ALL"
+  vpc_id               = aws_vpc.main.id
+}
