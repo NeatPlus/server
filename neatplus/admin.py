f331a731f023e3228e08ed7afea7293488e76c69
Saurav Sharma <saurav.sharma@nepware.com>
2023-01-16 14:31:40 +0545

    fix: use FK and M2M str value instead of pk
    Signed-off-by: Saurav Sharma <saurav.sharma@nepware.com>

--- neatplus/admin.py
+++ neatplus/admin.py
@@ -1,4 +1,8 @@
+import csv
+
 from django.contrib import admin
+from django.db import models
+from django.http import HttpResponse
 from django.http.response import HttpResponseRedirect
 from django.utils.translation import gettext_lazy as _
 
@@ -33,3 +37,35 @@ class AcceptRejectModelAdmin(admin.ModelAdmin):
             )
             return HttpResponseRedirect(".")
         return super().response_change(request, obj)
+
+
+class ExportCsvMixin:
+    def export_as_csv(self, request, queryset):
+        meta = self.model._meta
+        field_names = [field.name for field in meta.fields]
+        response = HttpResponse(content_type="text/csv")
+        response["Content-Disposition"] = f"attachment; filename={meta.model_name}.csv"
+        writer = csv.writer(response)
+
+        writer.writerow(field_names)
+        for obj in queryset:
+            row_values = []
+            for field in field_names:
+                field_value = getattr(obj, field)
+                value = None
+                if field == models.ForeignKey or models.OneToOneField:
+                    if field_value:
+                        value = str(field_value)
+                elif field == models.ManyToManyField:
+                    value = []
+                    if field_value:
+                        for val in field_value:
+                            value.append(str(val))
+                if value is None:
+                    value = field_value
+                row_values.append(value)
+            row = writer.writerow(row_values)
+
+        return response
+
+        export_as_csv.short_description = "Export Selected row"
